21ba8951871e6827ed738d60cb07dca8d9fa9896
JoshuaZheng <3107641910@qq.com>
2024-02-03 22:40:28 +0800

    feat: types chrome

--- config.ts
+++ config.ts
@@ -4,6 +4,25 @@ import path from 'path'
 /* 打包配置 */
 const env = loadEnv(process.env.mode, process.cwd());
 
+const build_watch: UserConfig = {
+  build: {
+    outDir: env.VITE_OUTDIR,
+    rollupOptions: {
+      input: {
+        popup: path.resolve(__dirname, "popup.html"),
+        content: path.resolve(__dirname, "src/__content/content.ts"),
+        background: path.resolve(__dirname, "src/__background/background.ts"),
+        option: path.resolve(__dirname, "src/__option/option.ts"),
+      },
+      output: {
+        chunkFileNames: "js/[name].[hash].js",
+        assetFileNames: "[ext]/[name].[ext]",
+        entryFileNames: "js/[name].js"
+      }
+    }
+  }
+}
+
 const content: UserConfig = {
   build: {
     outDir: env.VITE_CONTENT_OUTDIR,
@@ -57,7 +76,7 @@ const config = {
   defaultConfig,
 }
 
-export { content, background, option, sidepanel };
+export { content, background, option, sidepanel, build_watch };
 export default function getConfig(key: string) {
   if (config[key]) {
     return config[key];
